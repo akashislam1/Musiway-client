09e6002abc56ccf9957808a7c88642428dd3f651
akashislam1 <m.naimul110@gmail.com>
2023-06-12 20:09:52 +0600

    dark, light  toggle add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,19 +5,37 @@ import OurTeachers from "../OurTeachers/OurTeachers";
 import Program from "../Program/Program";
 import HeroSection from "../HeroSection/HeroSection";
 import About from "../About/About";
+import { darkTheme, lightTheme } from "../theme/Theme";
+import { useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Home = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+  const themeClass = isDarkTheme ? darkTheme : lightTheme;
   return (
-    <div>
-      <Helmet>
-        <title>Home - Musiway School </title>
-      </Helmet>
-      <Banner></Banner>
-      <About></About>
-      <OurClasses></OurClasses>
-      <OurTeachers></OurTeachers>
-      <Program></Program>
-      <HeroSection></HeroSection>
+    <div className={`transition duration-500 ${themeClass}`}>
+      <div>
+        <Helmet>
+          <title>Home - Musiway School </title>
+        </Helmet>
+        <button className="absolute z-10 top-7 right-48" onClick={toggleTheme}>
+          {isDarkTheme ? (
+            <FaMoon className="h-6 w-6 "></FaMoon>
+          ) : (
+            <FaSun className="h-6 w-6 text-white"></FaSun>
+          )}
+        </button>
+
+        <Banner></Banner>
+        <About></About>
+        <OurClasses></OurClasses>
+        <OurTeachers></OurTeachers>
+        <Program></Program>
+        <HeroSection></HeroSection>
+      </div>
     </div>
   );
 };
