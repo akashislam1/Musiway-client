ebe71f2ed7cff0354340a0b7b87fca1e3350d6ca
akashislam1 <m.naimul110@gmail.com>
2023-06-20 20:18:31 +0600

    change on classes card

--- src/Pages/ClassesPage/ClassesCard.jsx
+++ src/Pages/ClassesPage/ClassesCard.jsx
@@ -1,28 +1,43 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const ClassesCard = ({ classData, isAdmin, isInstructor, handleSelect }) => {
   const { image, title, price, availableSeats, instructor } = classData || {};
   return (
     <div
       className={` ${
         availableSeats === 0
-          ? "bg-red-600 p-5 flex flex-col justify-between gap-5 relative rounded-xl hover:shadow-xl text-white"
-          : "bg-white p-5 flex flex-col justify-between gap-5 relative rounded-xl hover:shadow-xl text-black"
+          ? "bg-red-600  flex flex-col justify-between  relative rounded-xl hover:shadow-xl text-white  w-full h-64"
+          : "bg-white flex flex-col justify-between gap-5 relative rounded-xl hover:shadow-xl text-black  w-full h-64"
       }`}
     >
-      <img className="w-32 " src={image} alt="" />
-      <div className="space-y-3">
-        <h3 className="text-2xl md:text-4xl font-bold ">{title}</h3>
-        <h5 className="text-[16px] font-semibold ">
+      <div
+        className={`${
+          availableSeats === 0
+            ? "bg-red-600 opacity-75 absolute w-full h-full rounded-2xl"
+            : "bg-black opacity-75 absolute w-full h-full rounded-2xl"
+        }`}
+      ></div>
+      <LazyLoadImage
+        effect="blur"
+        className="w-full h-full object-cover  rounded-2xl "
+        src={image}
+        alt=""
+      />
+      <div className="space-y-3 w-3/4  absolute top-16 left-8  ">
+        <h3 className="text-2xl md:text-3xl font-bold text-amber-400 ">
+          {title}
+        </h3>
+        <h5 className="text-[16px] font-semibold text-gray-200">
           Instructor : {instructor}
         </h5>
-        <p>Available Seats : {availableSeats}</p>
-        <p className="bg-black text-white absolute top-4 right-4 py-2 px-4 font-semibold rounded-md">
-          ${price}
-        </p>
+        <p className=" text-gray-200">Available Seats : {availableSeats}</p>
+
         <div>
           <button
             onClick={() => handleSelect(classData)}
-            className={`btn normal-case hover:bg-black btn-sm text-white bg-[#cc9724]  ${
-              availableSeats === 0 ? " disabled:text-black " : ""
+            className={`btn normal-case hover:bg-black border-0 btn-sm text-white bg-[#cc9724]  ${
+              availableSeats === 0
+                ? " disabled:text-white disabled:bg-slate-600"
+                : ""
             } `}
             disabled={
               classData.selectedClass === "selected" ||
@@ -35,6 +50,9 @@ const ClassesCard = ({ classData, isAdmin, isInstructor, handleSelect }) => {
           </button>
         </div>
       </div>
+      <p className="bg-white text-black absolute top-4 right-4 py-2 px-3 font-semibold rounded-md">
+        ${price}
+      </p>
     </div>
   );
 };
